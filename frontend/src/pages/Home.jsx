03218daa48b818bb7ad92f8c3a8c41bab6799733
iamlaurajp <lauratimburwa@gmail.com>
2026-08-12 18:13:27 +0200

    removed everything starting again

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  const movies = [
+    {id: 1, title: "She's The Man", release_date: "2020"}
+    {id: 1, title: "The Bee Movie", release_date: "2012"}
+    {id: 1, title: "", release_date: "2020"}
+  ]
+
+
+  return<div className="home"></div>
+}
