a91d4224b128041e24b42e7a08f1c6fa767f4f65
iamlaurajp <lauratimburwa@gmail.com>
2026-08-12 17:39:44 +0200

    created a componrnt in app.jsx

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,12 +1,21 @@
 
 function MovieCard({movie}){
 
+  function onFavoriteClick() {
+    alert("clicked")
+  }
+
   return <div className="movie-card">
     <div className="movie-poster">
       <img src={movie.url} alt={movie.title}/>
       <div className="movie-overlay">
-        <button className="favorite-btn" onClick={}></button>
+        <button className="favorite-btn" onClick={onFavoriteClick}></button>
       </div>
     </div>
+    <div className="movie-info"></div>
+    <h3>{movie.title}</h3>
+    <p>{movie.release_date}</p>
   </div>
 }
+
+export default MovieCard
