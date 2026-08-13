c42133b413e372c16e2244c8083245b86bb0da10
iamlaurajp <lauratimburwa@gmail.com>
2026-08-13 22:00:16 +0200

    added a form

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,24 @@
+import MovieCard from "../components/MovieCard";
+
 function Home() {
   const movies = [
-    {id: 1, title: "She's The Man", release_date: "2020"}
-    {id: 1, title: "The Bee Movie", release_date: "2012"}
-    {id: 1, title: "", release_date: "2020"}
-  ]
+    { id: 1, title: "She's The Man", release_date: "2005" },
+    { id: 2, title: "Bee Movie", release_date: "2020" },
+    { id: 3, title: "Avengers: Endgame", release_date: "2018" }
+  ];
 
+  return (
+    <div className="home">
+      <form onSubmit={} className="search-form"></form>
 
-  return<div className="home"></div>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
+
+export default Home;
+
